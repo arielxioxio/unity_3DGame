492d6b27b3e12e1dfed2fda5ee32c0cd66b8b31e
arielxioxio <71679945+arielxioxio@users.noreply.github.com>
2020-12-22 15:59:11 +0800

    9(碰撞)

---
+++ 3Dgame/Assets/scripts/NPC.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCData data;
+
+    public bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "怪人")
+        {
+            playerInArea = true;
+        Dialog()
+        }
+    } 
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.name=="怪人")
+        { 
+            playerInArea = false;
+        }
+    }
+    private void Dialog() 
+    {
+        print(data.dialougA);
+    }
+
+
+
+}
---
+++ 3Dgame/Assets/scripts/NPCData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NPC資料", menuName = "AYA/NPC 資料")]
+public class NPCData : ScriptableObject
+{
+    [Header("第一段對話"), TextArea(1, 5)]
+    public string dialougA;  
+    [Header("第二段對話"),TextArea(1,5)]
+    public string dialougB;
+    [Header("第三段對話"),TextArea(1,5)]
+    public string dialougC;
+    [Header("任務需求數量")]
+    public int count;
+    [Header("已經取得數量")]
+    public int countCurrent;
+
+}
